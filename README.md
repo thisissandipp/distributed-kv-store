3757a3b2da92d1075f346de243da9f5c05a2a378
Sandip <sandipramanik07@gmail.com>
2025-09-20 20:14:46 +0530

    initialize repository ✨

---
+++ README.md
@@ -0,0 +1,3 @@
+# Distributed Key-Value Store
+
+A system design project for building a distributed key-value store.
---
+++ src/main/java/com/example/distributed_kv_store/DistributedKvStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.distributed_kv_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedKvStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedKvStoreApplication.class, args);
+	}
+
+}
